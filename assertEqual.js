cfa8fe73f7baec490a4b0e38d2a3f37155dcc8ef
Jose Bautista <Cooldj1020@gmail.com>
2021-08-17 10:11:06 -0600

    using both testing functions

--- assertEqual.js
+++ assertEqual.js
@@ -7,5 +7,5 @@ const assertEqual = function (actual, expected) {
 };
 
 // TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
+assertEqual([1, 2, 3], [1, 2, 3]); // => true
+assertEqual([1, 2, 3], [3, 2, 1]);
--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,16 @@
 const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
+  return actual === expected
+    ? `✅✅✅  Assertion Passed: ${actual} === ${expected}`
+    : `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
+};
+const eqArrays = (a, b) => {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
 };
 
 // TEST CODE
-console.log(assertEqual(([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
