1b9e6019aa8d4ce8d6c9687e1f6a2df8bb8ec3a2
Jose Bautista <Cooldj1020@gmail.com>
2021-08-16 19:02:06 -0600

    using assertEqual with other functions

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = arr => arr.slice(1);
+
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result, ['Lighthouse', 'Labs']);
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
