beec2a01393b518f5b66003087fa69c80a66d514
Jose Bautista <Cooldj1020@gmail.com>
2021-08-18 15:44:11 -0600

    couting letters function

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,37 @@
-const countLetters = (str) => {
+const eqArrays = (a, b) => {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+};
+
+const assertArraysEqual = (a, b) => {
+  return eqArrays(a, b);
+};
+
+
+const letterPositions = (str) => {
   let obj = {};
-  const newStr = str.split(' ').join('').toLowerCase();
-  console.log(newStr);
-  for (const letter in newStr) {
-    obj[newStr[letter]] = newStr.split(newStr[letter]).length - 1;
+  const newStr = str.split(' ').join('').toLowerCase().trim();
+
+  for (let letter = 0; letter <= newStr.length - 1; letter++) {
+    if (obj[newStr[letter]]) {
+      obj[newStr[letter]].push(letter);
+    } else {
+      obj[newStr[letter]] = [letter];
+    }
   }
   return obj;
 };
-console.log(countLetters('lighthouse in the house'));
+assertArraysEqual(letterPositions('lighthouse in the house').l, [0]);
+assertArraysEqual(letterPositions('lighthouse in the house').i, [1, 11]);
+assertArraysEqual(letterPositions('lighthouse in the house').g, [2]);
+assertArraysEqual(letterPositions('lighthouse in the house').h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions('lighthouse in the house').t, [4, 14]);
+assertArraysEqual(letterPositions('lighthouse in the house').o, [6, 19]);
+assertArraysEqual(letterPositions('lighthouse in the house').u, [7, 20]);
+assertArraysEqual(letterPositions('lighthouse in the house').s, [8, 21]);
+assertArraysEqual(letterPositions('lighthouse in the house').e, [9, 16, 22]);
+assertArraysEqual(letterPositions('lighthouse in the house').n, [12]);
