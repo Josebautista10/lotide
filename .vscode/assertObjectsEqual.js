37b0ba139f8caf89b0e1555678e5f88b5638ca17
Jose Bautista <Cooldj1020@gmail.com>
2021-08-18 21:02:48 -0600

    testing assertObjectEqual

---
+++ .vscode/assertObjectsEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = (a, b) => {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+};
+
+const eqObjects = (object1, object2) => {
+  let arr1 = Object.entries(object1).flat(10).sort();
+  let arr2 = Object.entries(object2).flat(10).sort();
+
+  return eqArrays(arr1, arr2);
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
+  }  
+    return `❌❌❌ Assertion Failed ${inspect(actual)} !== ${inspect(expected)}`;
+  
+};
+
+console.log(
+  assertObjectsEqual({ c: '1', d: ['2', 3] }, { d: ['2', 3], c: '1' })
+);
---
+++ .vscode/eqObjects.js
@@ -0,0 +1,23 @@
+const eqArrays = (a, b) => {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+};
+
+const eqObjects = (object1, object2) => {
+
+  let arr1 = Object.entries(object1).flat(10).sort();
+  let arr2 = Object.entries(object2).flat(10).sort();  
+  
+  return eqArrays(arr1, arr2) 
+}
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc))
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); 
